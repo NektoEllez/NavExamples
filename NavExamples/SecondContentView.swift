a91a131a8fa608be2143de8bb711437c8827e514
Ivan Potyng <nekto_ellez@gmail.com>
2023-10-05 20:24:26 +0700

    Пример навигации с iOS 16

---
+++ NavExamples/SecondContentView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondContentView.swift
+//  NavExamples
+//
+//  Created by Иван Марин on 05.10.2023.
+//
+
+import SwiftUI
+
+struct SecondContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondContentView()
+}
