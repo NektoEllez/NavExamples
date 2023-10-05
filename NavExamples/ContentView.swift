3bab52e4ad5412ad065aba59b747a858bcbb8318
Ivan Potyng <nekto_ellez@gmail.com>
2023-10-05 17:00:07 +0700

    Initial Commit

---
+++ NavExamples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavExamples
+//
+//  Created by Иван Марин on 05.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavExamples/NavExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavExamplesApp.swift
+//  NavExamples
+//
+//  Created by Иван Марин on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
